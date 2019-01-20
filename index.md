c50d4507c35caa7d08927ff9237f07b214619f28
Tyler Williams <tyler@ogdenstudios.xyz>
2019-01-20 00:46:55 -0700

    font selector works on home page

--- index.md
+++ index.md
@@ -6,14 +6,14 @@ layout: page
 ---
 <div class="customizerr">
 <label for="font-select">Choose a font:</label>
- <select id="font-select" onchange="customize()">
+ <select id="font-select" onchange="customizeFont()">
   <option value="Work Sans">Work Sans</option>
   <option value="Roboto">Roboto</option>
   <option value="IBM Plex Sans">IBM Plex Sans</option>
 </select> 
 <br />
 <label for="color-select">Choose a color:</label>
- <select id="color-select" onchange="customize()">
+ <select id="color-select" onchange="customizeColor()">
   <option value="green">Green</option>
   <option value="magenta">Magenta</option>
   <option value="blue">Blue</option>
--- javascripts/customizerr.js
+++ javascripts/customizerr.js
@@ -1,7 +1,12 @@
-function customize() {
+function customizeFont() {
     var fontSelect = document.getElementById("font-select");
+    var selectedFont = fontSelect.options[fontSelect.selectedIndex].value;
+    var body = document.getElementsByTagName("BODY")[0]; 
+    body.style.fontFamily = selectedFont.toString();
+}
+
+function customizeColor() {
     var colorSelect = document.getElementById("color-select");
-    
-    console.log(fontSelect.options[fontSelect.selectedIndex].value);
-    console.log(colorSelect.options[colorSelect.selectedIndex].value);
+    console.log("Selected color is: " + colorSelect.options[colorSelect.selectedIndex].value);
+    console.log("Current color is: ");
 }
