17ae2b0ed2a58d556105bd9c19aadd5bbbc63f43
Tyler Williams <tyler@ogdenstudios.xyz>
2019-01-20 01:19:30 -0700

    customizerr works on home page

--- javascripts/customizerr.js
+++ javascripts/customizerr.js
@@ -7,6 +7,44 @@ function customizeFont() {
 
 function customizeColor() {
     var colorSelect = document.getElementById("color-select");
-    console.log("Selected color is: " + colorSelect.options[colorSelect.selectedIndex].value);
-    console.log("Current color is: ");
+    var selectedColor = colorSelect.options[colorSelect.selectedIndex].value;
+    var navLinks = document.getElementsByClassName("page-link");
+    var siteTitle = document.getElementsByClassName("site-title");
+    var footerLinks = document.getElementsByClassName("footer__link");
+    var links = document.getElementsByTagName("A"); 
+
+    for (link of navLinks) {
+        link.classList.remove("hover--blue");
+        link.classList.remove("hover--green");
+        link.classList.remove("hover--magenta");
+        link.classList.add("hover--" + selectedColor);
+    }
+    for (link of siteTitle) {
+        link.classList.remove("hover--blue");
+        link.classList.remove("hover--green");
+        link.classList.remove("hover--magenta");
+        link.classList.add("hover--" + selectedColor);
+    }
+    for (link of footerLinks) {
+        link.classList.remove("hover--blue");
+        link.classList.remove("hover--green");
+        link.classList.remove("hover--magenta");
+        link.classList.add("hover--" + selectedColor);
+    }
+    for (link of links) {
+        if (link.classList.length < 1) {
+        link.classList.remove("hover--blue");
+        link.classList.remove("hover--green");
+        link.classList.remove("hover--magenta");
+        link.classList.remove("color--blue");
+        link.classList.remove("color--green");
+        link.classList.remove("color--magenta");
+        link.classList.remove("visited--blue");
+        link.classList.remove("visited--green");
+        link.classList.remove("visited--magenta");
+        link.classList.add("color--" + selectedColor);
+        link.classList.add("hover--" + selectedColor);
+        link.classList.add("visited--" + selectedColor);
+        }   
+    }
 }
